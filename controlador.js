be66edbc3a60c16e64505a52b07c530ef110e6f2
valeriagran <70981308+valeriagran@users.noreply.github.com>
2021-07-21 16:53:11 -0500

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,7 @@
+//programa que nos ayude a establecer el costo total de una consola de video juegos
+
+//en el esterior 
+//copra en dolares 
+//pagar un casillero (dolares costo dependiendo de la cantidad de productos)
+//envio hasta colombia (cantidad de productos)
+//pagar impuestos de entrada al pais(lo digita aduanas)

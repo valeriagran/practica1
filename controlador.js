8bac7711c87c4e31646f414a9f382f1d6a749870
valebetancurgran <valebeta-8217@outlook.es>
2021-07-22 16:56:53 -0500

    controlador

--- controlador.js
+++ controlador.js
@@ -5,3 +5,8 @@
 //pagar un casillero (dolares costo dependiendo de la cantidad de productos)
 //envio hasta colombia (cantidad de productos)
 //pagar impuestos de entrada al pais(lo digita aduanas)
+
+
+let boton = document.getElementById("boton");
+console.log(boton);
+
